d2278729294c239689a1f20db2cca69df58ae81e
JC8398 <113597992+JC8398@users.noreply.github.com>
2022-09-15 11:53:48 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,4 +18,4 @@ In the meantime, below is an example of what you can do with just a few lines of
 
 
 st.write(pd.DataFrame({'first column': ['BBCM', 'Janes', 'Secret', 'Cardinal'],
-                   'second column':[np.random.randn(0,1), np.random.randn(0,1), np.random.randn(0,1), np.random.randn(0,1)]}))
+                   'second column':[0.6, 0.7, 0.98, 0.3]}))
